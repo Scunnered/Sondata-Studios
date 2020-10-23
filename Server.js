71220a7c7242e45174d8fab8031e5b7563a39a2c
1808097 <1808097@rgu.ac.uk>
2020-10-23 13:02:51 +0100

    Adding files to Test MongoDB. Basic server & html

---
+++ Server.js
@@ -0,0 +1,41 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/spectrograms";
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+var db;
+
+MongoClient.connect(url, function(err, database){
+    if(err) throw err;
+    db = database;
+    app.listen(8080);
+});
+
+app.get('/all', function(req, res) {
+    db.collection('spectrograms').find().toArray(function(err, result) {
+        if (err) throw err;
+        
+        var output = "<h1>All the data</h1>";
+        
+        for (var i = 0; i < result.length; i++) {
+            output += "<div>"
+            output += "<h3>" + result[i].test + "</h3>"
+            output += "<p>" + result[i].text + "</p>"
+            output += "</div>"
+        }
+        res.send(output);
+    });
+});
+
+app.post('/quotes', function (req, res) {
+    db.collection('spectrograms').save(req.body, function(err, result) {
+        if (err) throw err;
+        console.log('saved to database')
+        res.redirect('/')
+    })
+})
