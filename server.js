c784f6460f1be6270970f44e75627d6798d83159
1808097 <1808097@rgu.ac.uk>
2020-10-29 13:36:01 +0000

    Further attempt

--- server.js
+++ server.js
@@ -27,30 +27,31 @@ app.use(express.static('public'))
 
 //server.listen(8080); //listens on host 8080
 
+if (mongo) {
 
-var db;
-var makeswork;
-
-MongoClient.connect(url, function(err, database){
-    if(err) throw err;
-    db = database;
-    app.listen(8080);
-});
-
-
-app.get('/all', function(req, res) {
-    db.collection('spectrograms').find().toArray(function(err, result) {
-        if (err) throw err;
-        
-        var output = "<h1>All the data</h1>";
-        
-        for (var i = 0; i < result.length; i++) {
-            output += "<div>"
-            output += "<h3>" + result[i].test + "</h3>"
-            output += "<p>" + result[i].text + "</p>"
-            output += "</div>"
-        }
-        res.send(output);
+    var db;
+    var makeswork;
+    
+    MongoClient.connect(url, function(err, database){
+        if(err) throw err;
+        db = database;
+        app.listen(8080);
     });
-});
-
+    
+    
+    app.get('/all', function(req, res) {
+        db.collection('spectrograms').find().toArray(function(err, result) {
+            if (err) throw err;
+            
+            var output = "<h1>All the data</h1>";
+            
+            for (var i = 0; i < result.length; i++) {
+                output += "<div>"
+                output += "<h3>" + result[i].test + "</h3>"
+                output += "<p>" + result[i].text + "</p>"
+                output += "</div>"
+            }
+            res.send(output);
+        });
+    });
+}
