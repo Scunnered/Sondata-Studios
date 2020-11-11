c48334eeb22bcaf084c5468cff3ae4d63f4eccb5
1808097 <1808097@rgu.ac.uk>
2020-11-11 17:15:32 +0000

    deleting dot

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/spectrograms";
 const express = require('express');
-.
+
 const bodyParser = require('body-parser')
 const app = express();
 const server = require('http').Server(app);
