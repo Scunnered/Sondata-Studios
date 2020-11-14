7f7f8e54b30269acc23bd10b41f2f8fc6370340f
1808097 <1808097@rgu.ac.uk>
2020-11-14 11:44:44 +0000

    Changing typo

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -2,7 +2,7 @@ function getCanvas(){
     var CVS = document.getElementById("spectrogram");
     CVS.height = 10;
     CVS.width = 10;
-    var CTX = cvs.getContext("2d");
+    var CTX = CVS.getContext("2d");
     console.log(CVS)
     console.log(CTX)
     console.log(CVS.toDataURL())
